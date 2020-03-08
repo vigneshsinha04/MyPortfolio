b152fb34a25995735ba21d4050a89463e4e1c42d
Vignesh Sinha <vigneshsinha04@gmail.com>
2020-03-08 21:07:40 +0530

    IMDB changes

--- webscraping.py
+++ webscraping.py
@@ -1,24 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-import pprint
 import unicodecsv as csv
 import pandas as pd
 
-res = requests.get('https://news.ycombinator.com/news')
+res = requests.get('https://www.imdb.com/list/ls002448041/')
 soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.storylink')
-scores = soup.select('.score')
+links = soup.select('.lister-item-content')
 
-def curate_hacker_news(links,scores, filter_upvote):
+def curate_hacker_news(links, filter_upvote):
 	curate_list = []
 	for i in range(len(links)):
-		title = links[i].text
-		href = links[i].get('href', None)
-		points = scores[i].text
+		title = links[i].a.text
+		href = links[i].a.get('href', None)
+		points = links[i].find('span', class_ = 'ipl-rating-star__rating').text
 		if points:
-			range_scores = int(points.replace(' points', ''))
-			if range_scores > filter_upvote:
-				curate_list.append({'Title': title, 'Link': href, 'Upvotes': range_scores})
+			if float(points) > float(filter_upvote):
+				curate_list.append({'Title': title, 'Link': 'imdb.com'+href, 'Upvotes': points})
 	deceding_list_by_votes = sorted(curate_list, key = lambda k: k['Upvotes'], reverse=True)
 	return deceding_list_by_votes
 
@@ -27,7 +24,7 @@ def curated_links(votes):
 		fieldnames = ['Title', 'Link', 'Upvotes']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 		writer.writeheader()
-		for item in curate_hacker_news(links,scores, votes):
+		for item in curate_hacker_news(links, votes):
 			writer.writerow(item)
 	return 'Successfully completed!'
 
